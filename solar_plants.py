3bfef4a8fee3b549c17e2cbaba437fad96d37680
Ayatullakhan <124900200+Apeerkhan@users.noreply.github.com>
2023-08-13 16:08:59 +0530

    Add files via upload

---
+++ solar_plants.py
@@ -0,0 +1,14 @@
+import cv2
+cv2.imread("solar-system.jpg")
+cv2.imshow("output",img)
+cv2.waitKey(0)
+
+cv2.putText(img,
+            "Sun",
+            (20,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.8,
+            (255,255,255)
+            )
+
+cv2.imwrite("Solar_systemwithname.jpg",img)
